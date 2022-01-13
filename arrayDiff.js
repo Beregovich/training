1bf0c36ab186eba99fc5c63b9f3df5e682a4aa87
Beregovich <beregovich@mail.ru>
2022-01-13 15:31:40 +0300

    change console.log to return. Remove function squareDigits

--- arrayDiff.js
+++ arrayDiff.js
@@ -15,17 +15,5 @@ function arrayDiff(a, b) {
         }
     }
 
-    console.log(resultArr);
-}
-
-
-function squareDigits(num) {
-    let s = String(num);
-    let res;
-    for (let i = 0; i < s.length; i++) {
-        res = res + String(Number(s[i]) ** 2);
-    }
-
-
-    return Number(res);
+    return (resultArr);
 }
