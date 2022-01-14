2c2955d18e60cf9c37287362cb1e2da15e8711fe
Beregovich <beregovich@mail.ru>
2022-01-14 10:42:59 +0300

    resolve new task — maskify

---
+++ maskify.js
@@ -0,0 +1,19 @@
+/*Your task is to write a function maskify,
+which changes all but the last four characters into '#'.
+*/
+// return masked string
+function maskify(cc) {
+    var mask = "";
+    if (cc.length >= 5) {
+        for (let i = 0; i < cc.length - 4; i++) {
+            mask += "#";
+        }
+        mask += cc[cc.length - 4] + cc[cc.length - 3] + cc[cc.length - 2] + cc[cc.length - 1];
+        return mask;
+    } else {
+        mask = cc;
+        return mask;
+    }
+
+
+}
