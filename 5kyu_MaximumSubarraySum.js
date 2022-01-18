baf024fc8ec017004f83d9392c65b7437047d6f3
Beregovich <beregovich@mail.ru>
2022-01-18 12:39:50 +0300

    5kyu Maximum subarray sum. Not ready yet 2.

--- 5kyu_MaximumSubarraySum.js
+++ 5kyu_MaximumSubarraySum.js
@@ -3,18 +3,17 @@ var maxSequence = function (arr) {
     let max = arr[0];
     let end = arr[arr.length - 1];
     for (let i = 0; i <= end; i++) {
-        let k = maxSeq(arr.slice(i, end + 1));
+        let k = maxSeq(arr.slice(end - i + 1));
         if (k > max) {
             max = k;
         }
     }
     return max;
 }
-
 const maxSeq = function (subArr) {
     let max = subArr[0];
-    let kontainer;
-    for (let i = 0; i < subArr.length; i++) {
+    let kontainer = 0;
+    for (let i = 1; i < subArr.length; i++) {
         kontainer += subArr[i];
         if (max < kontainer) {
             max = kontainer;
