544d35c6ee03264d283d441ad4d89cc7005a8387
Beregovich <beregovich@mail.ru>
2022-02-06 23:58:45 +0300

    add 7kyu Exes and ohs another way

---
+++ 7kyu_ExesAndOhsCopy.ts
@@ -0,0 +1,13 @@
+//Check to see if a string has the same amount of 'x's and 'o's. 
+//The method must return a boolean and be case insensitive. The string can contain any char.
+function xo(str: string): boolean {
+    let xs: number= str.match(/[oO]/g).length;
+    let os: number= str.match(/[xX]/g).length;
+    console.log(xs)
+    console.log(os)
+    console.log(xs===os)
+    return (xs===os)
+}
+
+xo('XXxxOoO');
+//In my own case  match method with regular expression was didn't compiling. match was returned null
