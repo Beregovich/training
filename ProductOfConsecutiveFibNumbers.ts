38db0704d05fae41247ca5fc0bdddd61a568be91
Beregovich <beregovich@mail.ru>
2022-02-11 09:34:35 +0300

    5kyu add ProductOfConsecutiveFibNumbers.ts

---
+++ ProductOfConsecutiveFibNumbers.ts
@@ -0,0 +1,15 @@
+export class G964 {
+
+    public static productFib = (prod) => {
+      let fNumbers: number[]=[0,1]
+      let multiply: number=0;
+      let i: number=1;
+      while (multiply<=prod){
+        multiply=fNumbers[i]*fNumbers[i-1];
+        if (multiply===prod) return [fNumbers[i-1],fNumbers[i], true]
+        fNumbers.push(fNumbers[i]+fNumbers[i-1]);
+        i++
+      }
+      return [fNumbers[i-2],fNumbers[i-1], false]
+    }
+}
