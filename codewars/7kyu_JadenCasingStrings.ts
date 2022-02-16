2e9df7e33dfbd1e4f44e6313186c3024b5b2346f
Beregovich <beregovich@mail.ru>
2022-02-16 15:59:54 +0300

    7kyu Jaden Casing Strings with cycle

---
+++ codewars/7kyu_JadenCasingStrings.ts
@@ -0,0 +1,19 @@
+String.prototype.toJadenCase = function () {
+    let str : string = this;
+    let arr : string[] = [];
+    let i: number=1;
+    arr.push(str[0].toUpperCase())
+    while(i<str.length){
+        if( str[i]==" "){
+            arr.push(str[i]);
+            arr.push(str[i+1].toUpperCase())
+            i++;
+        }else
+            arr.push(str[i]);
+        i++;
+    }
+    return arr.join('')
+};
+
+//Use replace and RegExp
+
